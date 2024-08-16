788acd6b3542f13762d5cf1e8aedcf90ab600a32
Simon <Simon.torfs@gmail.com>
2024-08-16 08:00:32 +0200

    Analytics test

--- apps/client/main.tsx
+++ apps/client/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "../../style.css";
 import { BrowserRouter } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -13,6 +14,7 @@ try {
     <React.StrictMode>
       <BrowserRouter>
         <React.Suspense fallback="loading">
+          <Analytics />
           <App />
         </React.Suspense>
       </BrowserRouter>
