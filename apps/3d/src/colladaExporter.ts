b813258d3f34348b6c909a9d1bb0cb8331256f54
The_Raven <ravenvanhove@hotmail.be>
2024-08-20 11:03:59 +0200

    hellppp

---
+++ apps/3d/src/colladaExporter.ts
@@ -0,0 +1,8 @@
+import { Viewer } from './viewer';
+// create a collada file from the scene do NOT use the ColladaExporter class, create your own
+// use the xmlbuilder package to create the xml file
+// https://www.npmjs.com/package/xmlbuilder
+export function createCollada(viewer: Viewer): string {
+  return '';
+}
+
