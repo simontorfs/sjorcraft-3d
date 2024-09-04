52abd2ffc73399b49c5e20ff0eaf07e888dae9f9
simontorfs <simon.torfs@gmail.com>
2024-09-04 19:54:18 +0200

    Finding new lash candidates with lashtool

--- apps/3d/src/lashingTool.ts
+++ apps/3d/src/lashingTool.ts
@@ -1,5 +1,6 @@
 import { Pole } from "./pole";
 import { Viewer } from "./viewer";
+import * as THREE from "three";
 
 export class LashingTool {
   active: boolean;
@@ -23,7 +24,62 @@ export class LashingTool {
     console.log(this.hoveredPole);
   }
 
-  setHoveredPole(pole: Pole) {
-    this.hoveredPole = pole;
+  setHoveredPole(hoveredPole: Pole) {
+    this.hoveredPole = hoveredPole;
+    if (!hoveredPole) return;
+    //@ts-ignore
+    hoveredPole.mesh.material.color = new THREE.Color(0xffffff);
+
+    const point1 = this.viewer.inputHandler.getPointOnLineClosestToCursor(
+      hoveredPole.position,
+      hoveredPole.direction
+    );
+
+    let closestPole: Pole | undefined = undefined;
+    let closestPoleDist = Infinity;
+
+    const polesLashedToHovered = this.viewer.inventory.lashings.flatMap(
+      (lashing) => {
+        if (lashing.fixedPole === hoveredPole) return [lashing.loosePole];
+        if (lashing.loosePole === hoveredPole) return [lashing.fixedPole];
+        return [];
+      }
+    );
+
+    for (const pole of this.viewer.inventory.poles.filter(
+      (p) => p !== hoveredPole && !polesLashedToHovered.includes(p)
+    )) {
+      const poleDiff = hoveredPole.position.clone().sub(pole.position);
+      const polesDistance =
+        Math.abs(
+          hoveredPole.direction
+            .clone()
+            .cross(pole.direction)
+            .normalize()
+            .dot(poleDiff)
+        ) -
+        hoveredPole.radius -
+        pole.radius;
+
+      const point2 = this.viewer.inputHandler.getPointOnLineClosestToCursor(
+        pole.position,
+        pole.direction
+      );
+      const pointsDistance = point1.distanceTo(point2);
+
+      if (polesDistance < 0.2 && pointsDistance < polesDistance + 0.17) {
+        if (polesDistance < closestPoleDist) {
+          closestPoleDist = polesDistance;
+          closestPole = pole;
+        }
+      } else {
+        // @ts-ignore
+        pole.mesh.material.color = new THREE.Color(0xffffff);
+      }
+    }
+    if (closestPole) {
+      // @ts-ignore
+      closestPole.mesh.material.color = new THREE.Color(0x00ff00);
+    }
   }
 }
