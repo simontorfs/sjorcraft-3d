7fb9253e0c04e1e1c7ccda7f5b24d1f2a737ccf0
The_Raven <ravenvanhove@hotmail.be>
2024-09-13 13:12:31 +0200

    added faqpage and elements

--- apps/client/components/faq.tsx
+++ apps/client/components/faq.tsx
@@ -1,7 +1,30 @@
-import { Box, Paper, Typography } from "@mui/material";
+import { Box, Paper, Stack, Typography } from "@mui/material";
 import * as React from "react";
+import FaqQuestion from "./buildingBlocks/faqQuestion";
 
 export const Faq = () => {
+  const questions: { question: string; answer: string }[] = [
+    {
+      question: "What is Sjorcraft?",
+      answer:
+        "Sjorcraft is a web-based 3D editor for creating, sharing and exporting SJORconstructions.",
+    },
+    {
+      question: "Can I export my construction from Sjorcraft?",
+      answer:
+        "Yes, you can export your construction from Sjorcraft as different files (e.g. .sjor, .jpg, .stl, .dae) different filetypes are on the way. You can find the export button in the top right corner of the editor.",
+    },
+    {
+      question: "Is Sjorcraft free to use?",
+      answer:
+        'Yes, Sjorcraft is free to use. You can always support us by "buying us a coffee" on our website.',
+    },
+    {
+      question: "Can I cancel drawing a pole?",
+      answer:
+        "Yes, you can cancel drawing a pole by rightclicking while drawing. This action is possible while using the pole-, bipod- and tripodtool.",
+    },
+  ];
   return (
     <Box
       bgcolor={"Background"}
@@ -23,7 +46,16 @@ export const Faq = () => {
       >
         Frequently Asked Questions
       </Typography>
-      <Typography>Here are some frequently asked questions.</Typography>
+      <Stack
+        spacing={0}
+        sx={{
+          width: "80%",
+        }}
+      >
+        {questions.map((q, i) => (
+          <FaqQuestion key={i} question={q.question} answer={q.answer} />
+        ))}
+      </Stack>
     </Box>
   );
 };
