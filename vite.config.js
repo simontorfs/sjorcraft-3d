f201772899e9f1940a4d9fb2bb28c1db80802347
simontorfs <simon.torfs@gmail.com>
2024-09-22 11:05:34 +0200

    Configured vite to open with the debug hash

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ export default {
   base: "./",
   server: {
     host: true, // Open to local network and display URL
-    open: !("SANDBOX_URL" in process.env || "CODESANDBOX_HOST" in process.env), // Open if it's not a CodeSandbox
+    open: "http://localhost:5173/#debug",
   },
   build: {
     outDir: "./dist", // Output in the dist/ folder
